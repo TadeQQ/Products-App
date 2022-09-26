83aefb0097ba2ac428c5963175fe7e3dbb6ec655
Tadeusz <tadeuszkiszka1@gmail.com>
2022-09-26 18:57:12 +0200

    edit interface's key value, edit map method to map arr

--- components/list/ProductsList.tsx
+++ components/list/ProductsList.tsx
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { container, item, button } from "./Product.css";
+import React from "react";
+import { containerR, item } from "./Product.css";
 import Link from "next/link";
 import { Routes } from "../../routes/Routes";
 import { Product as ProductType } from "../../types/Product";
 import Image from "next/image";
 
 interface ProductListProps {
-  products: ProductType[];
+  products?: ProductType[][];
 }
 
 export const ProductsList = ({ products }: ProductListProps) => {
   return (
-    <div className={container}>
-      {products
-        ? products.map((product) => (
-            <Link href={Routes.PRODUCT(product.id)} key={product.id}>
-              <div className={item}>
-                <Image
-                  src={product.thumbnail}
-                  alt={product.title}
-                  height={200}
-                  width={220}
-                />
-                <div>
-                  <h3>{product.title}</h3>
-                  {product.description}
-                </div>
+    <div className={containerR}>
+      {products?.map((arr) =>
+        arr.map((product) => (
+          <Link href={Routes.PRODUCT(product.id)} key={product.id}>
+            <div className={item}>
+              <Image
+                src={product.thumbnail}
+                alt={product.title}
+                height={200}
+                width={220}
+              />
+              <div>
+                <h3>{product.title}</h3>
+                {product.description}
               </div>
-            </Link>
-          ))
-        : "Loading..."}
+            </div>
+          </Link>
+        ))
+      )}
     </div>
   );
 };
